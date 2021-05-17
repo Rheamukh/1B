d34c18c390aae2bd1788a53a0b24270bb95ec25c
Rheamukh <84300816+Rheamukh@users.noreply.github.com>
2021-05-17 08:13:42 -0500

    Constructor w/ Parameters
    Added a constructor that accepts all instance variables as parameters

--- Animal Farm/src/animals/Dog.java
+++ Animal Farm/src/animals/Dog.java
@@ -2,8 +2,23 @@ package animals;
 
 public class Dog 
 {
+	/**
+	 * @param breed
+	 * @param name
+	 * @param color
+	 * @param age
+	 */
+	public Dog(String breed, String name, String color, int age) 
+	{
+		this.breed = breed;
+		this.name = name;
+		this.color = color;
+		this.age = age;
+	}
 	private String breed;
 	private String name;
 	private String color;
 	private int age;
+	
+	
 }
